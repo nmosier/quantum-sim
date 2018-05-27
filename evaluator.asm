c2b2921e5f126a4959a1bb6c12faf17a1c64d168
Nicholas Mosier <nicholas@nicholass-air.middlebury.edu>
2018-05-26 22:49:55 -0400

    added append, insert, overwrite token functions

--- evaluator.asm
+++ evaluator.asm
@@ -4,44 +4,16 @@
 ;; Created by Nicholas Mosier on 05/13/2018
 
 ;; FLAGS: offset from SAFERAM appData
+#include "tables.asm"
+
 evalFlags .equ appData
 ketMode .equ 0 ; if SET, then disp state as ints; RESET, disp as bits
 
 inputBuffer_curP .equ appData+1
 inputBuffer_endP .equ appData+3
-scrap .equ appData+5
-scrapEnd .equ appData+11
-
-;; define structs
-QuditState_nbits .equ 0
-QuditState_nstates .equ 1
-QuditState_states .equ 2
-
-SBState_size .equ 3
-SBState_amp .equ 0
-SBState_val .equ 1
-
-;; QuditState layout
-;; - nbits: 1 byte
-;; - nstates: 1 byte
-;; - states: nstates bytes
-;; - amplitudes: nstates*18 bytes
-
-;; Qans qubit state used as accumulator
-
-#macro fill_word(count, data)
-	#if count > 0
-		.dw data
-		fill_word(count-1, data)
-	#endif
-#endmacro
-
-#macro pushw(imm16)
-	ld hl,imm16
-	push hl
-#endmacro
-
-
+inputBuffer_offsetE .equ appData+5	; relative from end, i.e. pos = endP - curP
+scrap .equ appData+7
+scrapEnd .equ appData+13
 
 inputBuffer .equ saveSScreen
 inputBuffer_end .equ saveSScreen+767	; reserve 767th byte for null-terminator
@@ -52,6 +24,8 @@ eval_get_input:
 	ld hl,inputBuffer
 	ld (inputBuffer_curP),hl ; reset input buffer pointer
 	ld (inputBuffer_endP),hl
+	ld hl,0
+	ld (inputBuffer_offsetE),hl
 
 	bcall(_ClrScrnFull)
 	ld hl,0
@@ -61,7 +35,7 @@ eval_get_input:
 
 eval_get_input_loop:
 	bcall(_GetKey)
-	ld de,input_JT2
+	ld de,input_JT
 	ld h,0
 	ld l,a
 	add hl,hl
@@ -73,111 +47,6 @@ eval_get_input_loop:
 	ld h,(hl)
 	ld l,e
 	jp (hl)
-
-input_JT:
-	.dw eval_handle_none	;$00
-	.dw eval_handle_arrow	;$01 V
-	.dw eval_handle_arrow	;$02 <
-	.dw eval_handle_arrow	;$03 >
-	.dw eval_handle_arrow	;$04 ^
-	fill_word($09-$04-1, eval_handle_none)
-	.dw eval_handle_enter	;$09 ENTER
-	.dw eval_handle_char	;$0A +
-	.dw eval_handle_char	;$0B -
-	fill_word($0F-$0B-1, eval_handle_none)
-	.dw eval_handle_clear	;$0F CLEAR
-	fill_word($11-$0F-1, eval_handle_none)
-	.dw eval_handle_char	;$11 -
-	.dw eval_handle_char	;$12 3
-	.dw eval_handle_char	;$13 6
-	.dw eval_handle_char	;$14 9
-	.dw eval_handle_char	;$15 )
-	fill_word($17-$15-1, eval_handle_none)
-	.dw eval_handle_vars	;$17 VARS
-	fill_word($19-$17-1, eval_handle_none)
-	.dw eval_handle_char	;$19 .
-	.dw eval_handle_char	;$1A 2
-	.dw eval_handle_char	;$1B 5
-	.dw eval_handle_char	;$1C 8
-	.dw eval_handle_char	;$1D (
-	fill_word($21-$1D-1, eval_handle_none)
-	.dw eval_handle_char	;$21 0
-	.dw eval_handle_char	;$22 1
-	.dw eval_handle_char	;$23 4
-	.dw eval_handle_char	;$24 7
-	fill_word($28-$24-1, eval_handle_none)
-	.dw eval_handle_gates	;$28 6
-	fill_word($36-$28-1, eval_handle_none)
-	.dw eval_handle_2nd		;$36 2md
-	fill_word($38-$36-1, eval_handle_none)
-	.dw eval_handle_del		;$38 DEL
-	
-; uses GetKey codes, not GetCSC
-input_JT2:
-	.dw eval_handle_none	;$00
-	.dw eval_handle_arrow	;$01
-	.dw eval_handle_arrow	;$02
-	.dw eval_handle_arrow	;$03
-	.dw eval_handle_arrow	;$04
-	.dw eval_handle_enter	;$05
-	fill_word($09-$05-1, eval_handle_none)
-	.dw eval_handle_clear	;$09
-	.dw eval_handle_del		;$0A
-	fill_word($0B-$0A-1, eval_handle_none)
-	.dw eval_handle_insert	;$0B
-	.dw eval_handle_none	;$0C
-	.dw eval_handle_none	;$0D
-	.dw eval_handle_none	;$0E
-	.dw eval_handle_none	;$0F
-	fill_word($2E-$0F-1, eval_handle_none)
-	.dw eval_handle_none	;$2E
-	.dw eval_handle_none	;$2F
-	fill_word($31-$2F-1, eval_handle_none)
-	.dw eval_handle_none	;$31
-	.dw eval_handle_none	;$32
-	fill_word($35-$32-1, eval_handle_none)
-	.dw eval_handle_none	;$35
-	fill_word($44-$35-1, eval_handle_none)
-	.dw eval_handle_none	;$44
-	.dw eval_handle_none	;$45
-	fill_word($48-$45-1, eval_handle_none)
-	.dw eval_handle_none	;$48
-	.dw eval_handle_none	;$49
-	fill_word($5A-$49-1, eval_handle_none)
-	.dw eval_handle_none	;$5A
-	fill_word($80-$5A-1, eval_handle_none)
-	.dw eval_handle_char	;$80 +
-	.dw eval_handle_char	;$81 -
-	.dw eval_handle_char	;$82 *
-	.dw eval_handle_char	;$83 /
-	fill_word($85-$83-1, eval_handle_none)
-	.dw eval_handle_char	;$85 (
-	.dw eval_handle_char	;$86 )	
-	fill_word($8A-$86-1, eval_handle_none)
-	.dw eval_handle_none	;$8A
-	.dw eval_handle_none	;$8B
-	fill_word($97-$8C+1, eval_handle_char)
-	fill_word($B4-$97-1, eval_handle_none)
-	.dw eval_handle_none	;$B4
-	fill_word($B7-$B4-1, eval_handle_none)
-	.dw eval_handle_none	;$B7
-	fill_word($B9-$B7-1, eval_handle_none)
-	.dw eval_handle_none	;$B9
-	fill_word($BB-$B9-1, eval_handle_none)
-	.dw eval_handle_none	;$BB
-	fill_word($BD-$BB-1, eval_handle_none)
-	.dw eval_handle_none	;$BD
-	.dw eval_handle_none	;$BF
-	fill_word($C1-$BF-1, eval_handle_none)
-	.dw eval_handle_none	;$C1
-	fill_word($FB-$C1, eval_handle_none)
-
-
-
-
-
-	
-
 
 eval_handle_none:
 	ret
@@ -198,54 +67,51 @@ _	bcall(_CursorOn)
 	ret
 	
 eval_move_cursor_right:
-	ld hl,(inputBuffer_endP)
-	ld de,(inputBuffer_curP)
-	inc de	;; tenative new cursor ptr
+	ld hl,(inputBuffer_offsetE)
+	ld a,h
+	or l
+	ret z	; return if already at end of input
+	dec hl
+	ld (inputBuffer_offsetE),hl
+	;; otherwise, find len of current token
+	ld hl,(inputBuffer_curP)
+	ld a,(hl)
+	inc hl
+	ld (inputBuffer_curP),hl
+	call tok2titok
+	bcall(_GetTokLen)
+	ld d,a
+	ld e,0
+	ld hl,(curRow)
+	add hl,de
+	call cursor_adjust
+	ld (curRow),hl
+	ret
+
+eval_move_cursor_left:
+	ld hl,(inputBuffer_curP)
+	ld de,inputBuffer
 	ld a,d
 	cp h
 	jr c,_
-	ld a,l
-	cp e
-	ret c	; if curP > endP, then abort
-_	ld hl,(curRow)
-	ld a,h
-	cp 15	; curCol = 15?
-	jr nz,_
-	ld a,l
-	cp 7	; curRow = 7?
-	call z,eval_scroll_down	;(15,7) -> need to scroll down
-	ld hl,(curRow)
-	ld a,l
-	cp 7
-	ret z
-	inc l	; curRow++
-	ld h,-1	; curCol=-1
-_	inc h	; curCol++
-	ld (curRow),hl
-	ld de,(inputBuffer_curP)
-	inc de
-	ld (inputBuffer_curP),de
-	ret
-	
-eval_move_cursor_left:
-	ld hl,(curRow)
-	ld a,h
-	or l
-	call z,eval_scroll_up
-	ld hl,(curRow)
-	ld a,h
-	or l
-	ret z
-	ld a,h
-	or a
-	jr nz,_	; skip if col > 0
-	ld h,16
-	dec l	; row--
-_	dec h	; col--
-	ld (curRow),hl
-	ld hl,(inputBuffer_curP)
+	ld a,e
+	cp l
+	ret nc	; check that cursor not at start
+_	
 	dec hl
 	ld (inputBuffer_curP),hl
+	ld a,(hl)
+	call tok2titok
+	bcall(_GetTokLen)
+	ld hl,(curRow)
+	neg
+	add a,h
+	ld h,a
+	call cursor_adjust
+	ld (curRow),hl
+	ld hl,(inputBuffer_offsetE)
+	inc hl
+	ld (inputBuffer_offsetE),hl
 	ret
 
 eval_move_cursor_down:
@@ -294,26 +160,39 @@ eval_handle_enter:
 	ret
 
 eval_handle_insert:
-eval_handle_2nd:
 	ld a,(iy+textFlags)
 	xor 1<<textInsMode
 	ld (iy+textFlags),a
 	ret
 
+;CONVERTED
 eval_handle_char:
-	; a = CSC scan code
-	ld hl,eval_k2char_LUT
-	ld d,0
-	ld e,a
+	; a = GetKey scan code
+	ld b,a
+	ld de,(inputBuffer_curP)
+	ld hl,-inputBuffer_end
 	add hl,de
-	ld a,(hl)
+	ld a,h
+	or l
+	ret z	;; return if already maxed out buffer
+	
+	ld hl,k2tok_LUT
+	ld d,0
+	ld e,b
+	add hl,de
+	ld b,(hl)
 	
 	ld hl,_
 	push hl
 	bcall(_CursorOff)	
+	ld hl,(inputBuffer_offsetE)
+	ld a,h
+	or l
+	ld a,b
+	jp z,eval_append_tok
 	bit textInsMode,(iy+textFlags)
-	jp z,eval_overwrite_char
-	jp eval_insert_char
+	jp z,eval_overwrite_tok
+	jp eval_insert_tok
 _	bcall(_CursorOn)
 	ret
 
@@ -376,130 +255,130 @@ eval_handle_clear:
 	bcall(_CursorOn)
 	ret
 
-eval_handle_braket:
-	ret
-
-eval_k2char_LUT:
-	.fill $80,'?'	;$00-$80
-	.db '+'			;$80
-	.db '-'			;$81
-	.db '*'			;$82
-	.db '/'			;$83
-	.db '?'			;$84
-	.db '('			;$85
-	.db ')'			;$86
-	.fill $8C-$86-1,'?'
-	.db '-'			;$8C
-	.db '.'			;$8D
-	.db '0'			;$8E
-	.db '1'			;$8F
-	.db '2'			;$90
-	.db '3'			;$91
-	.db '4'			;$92
-	.db '5'			;$93
-	.db '6'			;$94
-	.db '7'			;$95
-	.db '8'			;$96
-	.db '9'			;$97
-	.fill $FB-$97,'?'
-
-eval_overwrite_char:
-	; INPUT: a = char to put
-	ld b,a
-	ld de,(inputBuffer_endP)
-	ld hl,-inputBuffer_end
-	add hl,de
-	ld a,h
-	or l
-	ret z
-	ld a,b
-	
-	bcall(_PutC)
-	;; update buffer
+; DONE
+eval_append_tok:	
 	ld hl,(inputBuffer_curP)
-	ld de,(inputBuffer_endP)
-	ld (hl),a	; store character into buffer
+	ld (hl),b	; store new token
 	inc hl
-	ld a,h	; check if curP > endP
-	cp d
-	jr c,eval_overwrite_char_more
-	jr nz,_
-	ld a,e
-	cp l
-	jr nc,eval_overwrite_char_more	; if curP <= endP, skip
-_	ld (inputBuffer_endP),hl ;; if hl=de, also update endP
-	ld (hl),0	; update end
-eval_overwrite_char_more:
 	ld (inputBuffer_curP),hl
-	;; check cursor position
-	ld a,(curRow)
-	cp 8
-	call z,eval_scroll_down	; if row=8, scroll down
+	ld (inputBuffer_endP),hl
+	ld (hl),0
+	ld a,b
+	call tok2titok	; get ti-tok to print
+	bcall(_PutTokString)
 	ret
 
-eval_insert_char:
-	; INPUT: a = char to insert
-	ld b,a
-	ld de,(inputBuffer_endP)
-	ld hl,-inputBuffer_end
-	add hl,de
-	ld a,h
-	or l
-	ret z
-	ld a,b
-	
-	ld e,a
-	
-	ld bc,(inputBuffer_curP)
-	ld a,$FF
-	xor b
-	ld b,a
-	ld a,$FF
-	xor c
-	ld c,a
-	inc bc ;; bc = -curPtr
-	ld hl,(inputBuffer_endP)
-	add hl,bc	; inputBuffer_endP - inputBuffer_curP
-	ld b,h
-	ld c,l
-	ld a,h
-	or l
-	ld a,e
-	jr z,eval_overwrite_char
-	
-	push af
-	
-	ld de,(inputBuffer_endP)
-	inc de
-	xor a
-	ld (de),a
-	dec de
-	ld h,d
-	ld l,e	; hl = de
+eval_overwrite_tok:
+	;; INPUT: a = tok to overwrite with
+	;; * assumes inputBuffer_curP < inputBuffer_endP *
+	ld (scrap),a	; save new token
+	call tok2titok
+	bcall(_GetTokLen)
+	ld (scrap+1),a	;save new token len
+	ld hl,(inputBuffer_curP)
+	ld a,(hl)
+	call tok2titok
+	bcall(_GetTokLen)
+	ld hl,scrap+1
+	sub (hl)
+	ld (hl),a	; scrap+1 contains # of spaces to write at end
 	dec hl
-	lddr
-	
-	pop af
-	
-	ld hl,(inputBuffer_endP)
-	inc hl
-	ld (inputBuffer_endP),hl
+	ld a,(hl)	; load new token
 	ld hl,(inputBuffer_curP)
 	ld (hl),a
-	bcall(_PutC)
 	inc hl
-	ld (inputBuffer_curP),hl
-	ld de,(curRow)	; preserve  cursor loc
-	bcall(_PutS)
-	ld (curRow),de
-	
-	;; carry flag reset (nc) if full str didn't display
-	;; in which case, recurse IF (curRow != 0)
-	ret c
-	ld a,(curRow)
+	ld (inputBuffer_curP),hl	; update cursor
+	call tok2titok
+	bcall(_PutTokString)
+	ld hl,(inputBuffer_offsetE)
+	dec hl
+	ld (inputBuffer_offsetE),hl	; update end offset
+	ld a,(scrap+1)	; a = # of spaces to write
 	or a
-	ret z	; return if row = 0; can't scroll
-	jr eval_scroll_down
+	ret z	; if old & new token lengths were same, then done
+	;; otherwise need to update rest of display
+	;; hl = offset
+	ex de,hl	; use de as loop counter, note de > 0
+	ld hl,(curRow)
+	push hl		; save cursor position
+	ld a,d
+	or e
+	jr z,_
+	ld hl,(inputBuffer_curP)
+eval_overwrite_tok_disploop:
+	push de
+	push hl
+	ld a,(hl)
+	call tok2titok
+	bcall(_PutTokString)
+	pop hl
+	pop de
+	inc hl
+	dec de
+	ld a,d
+	or e
+	jr nz,eval_overwrite_tok_disploop
+
+_	ld a,(scrap+1)	; # of spaces to write
+	bit 7,a			; test if a is negative
+	jr nz,_
+	ld b,a
+eval_overwrite_tok_spaces:
+	ld a,' '
+	bcall(_PutC)
+	djnz eval_overwrite_tok_spaces
+	
+_	pop hl
+	ld (curRow),hl	; restore cursor position
+	ret
+
+
+eval_insert_tok:
+	; input: a = tok to insert
+	; * guaranteed that curP not equal to endP
+	ld b,a
+	ld hl,(inputBuffer_endP)
+	ld de,inputBuffer_end	;; compare endP to end
+	ld a,h
+	cp d
+	jr c,_
+	ld a,l
+	cp e
+	ret nc	; return if endP ≥ end
+_	ld a,b
+	ld d,h
+	ld e,l
+	inc de
+	ld (inputBuffer_endP),de
+	ld bc,(inputBuffer_offsetE)
+	inc bc
+	lddr
+	ld (de),a
+	inc de
+	ld (inputBuffer_curP),de
+	call tok2titok
+	bcall(_PutTokString)	; display new token
+	ld hl,(inputBuffer_curP)
+	ld bc,(inputBuffer_offsetE)
+	ld de,(curRow)
+	push de
+eval_insert_tok_disploop:
+	;; hl ptr to tok
+	push hl
+	push bc
+	ld a,(hl)
+	call tok2titok
+	bcall(_PutTokString)
+	pop bc
+	pop hl
+	inc hl
+	dec bc
+	ld a,b
+	or c
+	jr nz,eval_insert_tok_disploop
+	pop de
+	ld (curRow),de
+	ret
 
 	
 eval_scroll_up:
